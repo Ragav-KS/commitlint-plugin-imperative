944e3c9738a8eeb3d5738b62b5b865116a3dcd50
Ragav-KS <57057039+Ragav-KS@users.noreply.github.com>
2025-02-06 23:33:08 +0530

    chore: configure my plugin into commitlint config

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,1 +1,9 @@
-export default { extends: ['@commitlint/config-conventional'] };
+import type { UserConfig } from '@commitlint/types';
+import myPlugin from './src/index';
+
+const config: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  plugins: [myPlugin],
+};
+
+export default config;
