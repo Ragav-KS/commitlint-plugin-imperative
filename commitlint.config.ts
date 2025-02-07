4ce2cc5c089f81e4eca4fea0a94d93180c48f6ea
Ragav-KS <57057039+Ragav-KS@users.noreply.github.com>
2025-02-07 12:36:23 +0530

    chore: integrate the rule into commitlint

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,10 +1,12 @@
 import type { UserConfig } from '@commitlint/types';
 import myPlugin from './src/index';
 
-const config: UserConfig = {
+const config = {
   extends: ['@commitlint/config-conventional'],
-  rules: { 'imperative-subject': [1, 'always'] },
+  rules: {
+    'imperative-subject': [2, 'always', { debug: true }],
+  },
   plugins: [myPlugin],
-};
+} satisfies UserConfig;
 
 export default config;
