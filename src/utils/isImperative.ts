c6fd79ea2aa6794f889bb01c7925279989a0e97a
Ragav-KS <57057039+Ragav-KS@users.noreply.github.com>
2025-02-17 12:15:55 +0530

    fix(extension): augment compromise's core logic (#22)
    ## Description
    
    Now the custom logic to check if the first term is a verb with present
    tense is an augment to compromise's native isImperative checker.
    
    Closes #21

--- src/utils/isImperative.ts
+++ src/utils/isImperative.ts
@@ -15,24 +15,21 @@ export function isImperative(
 ): IsImperativeResult {
   const doc = nlp(sentence);
 
+  if (debug) {
+    console.log('terms', doc.docs.flat());
+  }
+
   let firstTerm = doc.terms().first(); // Get the first word
 
   const isFirstTermVerb = firstTerm.has('#Verb');
   const isFirstTermPresentTense = firstTerm.has('#PresentTense');
 
-  const isImperative = isFirstTermVerb && isFirstTermPresentTense;
+  const isImperative =
+    doc.verbs().isImperative().found ||
+    (isFirstTermVerb && isFirstTermPresentTense);
 
   if (isImperative) {
     return { isImperative: true };
-  }
-
-  if (debug) {
-    console.log(
-      'isImperative according to compromise?',
-      doc.verbs().isImperative().found,
-    );
-
-    console.log('terms', doc.docs.flat());
   }
 
   if (isFirstTermVerb) {
